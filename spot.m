d3cf4346fe60db13542016ed038464234040c15f
joniere <675271277@qq.com>
2021-04-20 08:41:46 +0800

    质点的类

---
+++ spot.m
@@ -0,0 +1,27 @@
+classdef spot < handle
+    properties
+        speed
+        acceleration
+        direction  %方向
+        position
+    end
+    methods
+        function obj = spot(speed,acceleration,direction,position)
+            obj.speed=speed;
+            obj.acceleration = acceleration;
+            obj.direction=direction;
+            obj.position=position;
+        end
+        function forward(obj,time)
+            obj.position(1)=obj.position(1)+cos(obj.direction)*obj.speed*time;
+            obj.position(2)=obj.position(2)+sin(obj.direction)*obj.speed*time;
+            % 定义前进方向和距离
+            obj.speed=obj.acceleration*time+obj.speed;
+        end
+        %function jiasudu(obj,time)
+        %obj.speed=obj.accleration*time+obj.speed;
+        %end
+    end
+end
+
+
